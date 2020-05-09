59c349135d126586834f644a12c6edeac41ccfab
ccota <crcotag@gmail.com>
2020-05-08 20:35:40 -0700

    format code

--- day1/script.js
+++ day1/script.js
@@ -1,20 +1,19 @@
 const playSound = function (e) {
-    var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    var button = document.querySelector(`.button[data-key="${e.keyCode}"]`);
-    if (!audio)
-        return;
-    audio.currentTime = 0;
-    audio.play();
-    button.classList.add('playing');
+  var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  var button = document.querySelector(`.button[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  button.classList.add("playing");
 };
 
 const removePlayingClass = function (e) {
-    if (e.propertyName == 'transform') {
-        this.classList.remove('playing');
-    }
+  if (e.propertyName == "transform") {
+    this.classList.remove("playing");
+  }
 };
 
 window.addEventListener("keydown", playSound);
-document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('transitionend', removePlayingClass)
-})
+document.querySelectorAll(".button").forEach((button) => {
+  button.addEventListener("transitionend", removePlayingClass);
+});
