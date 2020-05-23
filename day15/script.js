54d6b1cb3426c205293070aa4b3fdf5d98630ff6
ccota <crcotag@gmail.com>
2020-05-23 12:46:03 -0700

    add styles to day 15

--- day15/script.js
+++ day15/script.js
@@ -1,5 +1,8 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
+const checkAll = document.querySelector("#checkAll");
+const uncheckAll = document.querySelector("#uncheckAll");
+const clearList = document.querySelector("#clearList");
 const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function addItem(e) {
@@ -19,26 +22,57 @@ function addItem(e) {
 }
 
 function populateList(plates = [], platesList) {
-  platesList.innerHTML = plates.map((plate, i) => {
-    return `
-      <li>
-        <input type="checkbox" data-index="${i}" id="items${i}" ${plate.done ? 'checked' : ''} />
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+        <li>
+        <input type="checkbox" data-index="${i}" id="items${i}" ${
+        plate.done ? "checked" : ""
+      } />
         <label for="items${i}">${plate.text}</label>
-      </li>
-    `;
-  }).join('');
+        </li>
+        `;
+    })
+    .join("");
 }
 
 function toggleDown(e) {
-    if(!e.target.matches('input')) return;
-    const element = e.target;
-    const i = element.dataset.index;
-    items[i].done = !items[i].done;
-    localStorage.setItem('items', JSON.stringify(items));
-    // populateList(items, itemsList);
+  if (!e.target.matches("input")) return;
+  const element = e.target;
+  const i = element.dataset.index;
+  items[i].done = !items[i].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function handlecheckAll(e) {
+  e.preventDefault();
+  items.forEach((item) => {
+    item.done = true;
+  });
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function handleuncheckAll(e) {
+  e.preventDefault();
+  items.forEach((item) => {
+    item.done = false;
+  });
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function handleclearList(e) {
+  e.preventDefault();
+  localStorage.setItem("items", JSON.stringify([]));
+  populateList([], itemsList);
 }
 
 addItems.addEventListener("submit", addItem);
 itemsList.addEventListener("click", toggleDown);
+checkAll.addEventListener("click", handlecheckAll);
+uncheckAll.addEventListener("click", handleuncheckAll);
+clearList.addEventListener("click", handleclearList);
 
 populateList(items, itemsList);
