0c28be5c8fdd33475aceb7bea3b413f7938185aa
ccota <crcotag@gmail.com>
2020-05-28 21:59:28 -0700

    rollback changes to original

--- day21/script.js
+++ day21/script.js
@@ -1,23 +1,10 @@
-const arrow = document.querySelector(".arrow");
-const speed = document.querySelector(".speed-value");
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
 
-navigator.geolocation.watchPosition(
-  (position) => {
-    let myObj = {};
-    myObj.accuracy = position.coords.accuracy;
-    myObj.altitude = position.coords.altitude;
-    myObj.altitudeAccuracy = position.coords.altitudeAccuracy;
-    myObj.heading = position.coords.heading;
-    myObj.latitude = position.coords.latitude;
-    myObj.longitude = position.coords.longitude;
-    myObj.speed = position.coords.speed;
-    // console.log(JSON.stringify(myObj));
-    speed.textContent = JSON.stringify(myObj);
-    // speed.textContent = position.coords;
-    // arrow.style.transform = `rotate(${position.coords.heading}deg)`;
-  },
-  (err) => {
-    speed.textContent = err.message;
-    console.error(err);
-  }
-);
+navigator.geolocation.watchPosition((position) => {
+  console.log(position);
+  speed.textContent = position.coords.speed;
+  arrow.style.transform = `rotate(${position.coords.heading}deg)`;
+}, (err) => {
+  console.error(err);
+});
