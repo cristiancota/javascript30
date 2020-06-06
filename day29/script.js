47133a98fae4f05e7d079ba4254bb72771510227
ccota <crcotag@gmail.com>
2020-06-05 20:51:58 -0700

    countdown

---
+++ day29/script.js
@@ -0,0 +1,53 @@
+let countdownInterval;
+const timerDisplay = document.querySelector(".timer-display");
+const endTime = document.querySelector(".end-time");
+const buttons = document.querySelectorAll("button");
+
+function timer(seconds) {
+  clearInterval(countdownInterval);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdownInterval = setInterval(function () {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdownInterval);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secondsLeft = seconds % 60;
+  const display = `${minutes}:${secondsLeft < 10 ? "0" : ""}${secondsLeft}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `See you at ${hour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach((button) => button.addEventListener("click", startTimer));
+
+document.customForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
